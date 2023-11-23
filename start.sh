45b436e1000a0cbbed2949bd18e35f32bb87682d
huyunhe <huyunhe@sparkedu.com>
2023-11-23 15:14:56 +0800

    进入保护模式

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ nasm -I include/ -o mbr.bin mbr.S
 nasm -I include/ -o loader.bin loader.S
 
 dd if=./mbr.bin of=./hd60M.img bs=512 count=1 seek=0 conv=notrunc
-dd if=./loader.bin of=./hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=./loader.bin of=./hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
 bochs -f ./bochsrc.disk -q
