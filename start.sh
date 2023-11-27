551111e037227a98ea95a2f3b6eceda45958d009
huyunhe <huyunhe@sparkedu.com>
2023-11-27 14:28:41 +0800

    fix: 检测内存大小

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-rm -rf mbr.bin
-rm -rf loader.bin
-
 nasm -I include/ -o mbr.bin mbr.S
 nasm -I include/ -o loader.bin loader.S
 
 dd if=./mbr.bin of=./hd60M.img bs=512 count=1 seek=0 conv=notrunc
 dd if=./loader.bin of=./hd60M.img bs=512 count=4 seek=2 conv=notrunc
+
+rm -rf mbr.bin
+rm -rf loader.bin
 
 bochs -f ./bochsrc.disk -q
